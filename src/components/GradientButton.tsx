858dc3a9eac79af039ad7a0b361ea3290a7895de
Stoil100 <toli100ilov@gmail.com>
2024-03-06 12:10:45 +0200

    Feat: Create gradient button for all around use

---
+++ src/components/GradientButton.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Button } from "./ui/button";
+import { cn } from "@/lib/utils";
+
+type Props = {
+    children: React.ReactNode;
+    className?: string;
+    buttonClassName?: string;
+    onClick?: () => {};
+};
+
+export default function GradientButton({
+    children,
+    className,
+    buttonClassName,
+    onClick,
+}: Props) {
+    return (
+        <Button
+            onClick={onClick}
+            className={cn(
+                "h-fit rounded-full bg-white transition-transform duration-300 hover:scale-[1.1] hover:bg-white",
+                buttonClassName,
+            )}
+        >
+            <p
+                className={cn(
+                    "text-light bg-gradient-to-b from-[#23A53D] to-[#6DD864] bg-clip-text text-transparent",
+                    className,
+                )}
+            >
+                {children}
+            </p>
+        </Button>
+    );
+}
