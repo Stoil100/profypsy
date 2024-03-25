8fef20fd35d2b06d884f3ee75c817f54f354fc71
Stoil100 <toli100ilov@gmail.com>
2024-03-25 23:21:11 +0200

    Chore: Change gradient button structure

--- src/components/GradientButton.tsx
+++ src/components/GradientButton.tsx
@@ -5,32 +5,23 @@ import { cn } from "@/lib/utils";
 type Props = {
     children: React.ReactNode;
     className?: string;
-    buttonClassName?: string;
     onClick?: () => void | (()=>{})|Function;
 };
 
 export default function GradientButton({
     children,
     className,
-    buttonClassName,
     onClick,
 }: Props) {
     return (
         <Button
             onClick={onClick}
             className={cn(
-                "h-fit rounded-full bg-white transition-transform duration-300 hover:scale-[1.1] hover:bg-white drop-shadow-lg",
-                buttonClassName,
+                "text-light flex h-fit items-center rounded-full bg-white  text-[#25BA9E] drop-shadow-lg transition-transform duration-300 hover:scale-[1.1] hover:bg-white",
+                className,
             )}
         >
-            <p
-                className={cn(
-                    "text-light text-[#25BA9E]",
-                    className,
-                )}
-            >
-                {children}
-            </p>
+            {children}
         </Button>
     );
 }
