d754b151c62687bb934740a2af1dd480e9827e54
Stoil100 <toli100ilov@gmail.com>
2024-04-10 22:42:14 +0300

    Fix: User unable to login

--- src/components/forms/auth.tsx
+++ src/components/forms/auth.tsx
@@ -24,7 +24,7 @@ type FormVariant = {
     variant: "register" | "login";
 };
 
-const AuthForm = ({ variant="login" }: FormVariant) => {
+const AuthForm = ({ variant = "login" }: FormVariant) => {
     const formSchema = z
         .object({
             email: z
@@ -46,8 +46,9 @@ const AuthForm = ({ variant="login" }: FormVariant) => {
                     .min(8, "Must be at least 8 characters in length"),
             }),
         })
+
         .superRefine(({ confirmPassword, password }, ctx) => {
-            if (confirmPassword !== password) {
+            if (confirmPassword !== password && variant === "register") {
                 ctx.addIssue({
                     code: "custom",
                     message: "The passwords did not match",
@@ -71,7 +72,6 @@ const AuthForm = ({ variant="login" }: FormVariant) => {
     });
 
     const onSubmit = async (values: z.infer<typeof formSchema>) => {
-        console.log(values as AuthT);
         setIsLoading(true);
         try {
             let errorMessage: any;
@@ -83,6 +83,70 @@ const AuthForm = ({ variant="login" }: FormVariant) => {
             if (errorMessage !== null) {
                 const errorMessageMap: Record<string, string> = {
                     "auth/email-already-in-use": "Email is already in use",
+                    "auth/user-not-found": "User does not exist",
+                    "auth/claims-too-large": "Claims payload is too large",
+                    "auth/id-token-expired": "ID token has expired",
+                    "auth/id-token-revoked": "ID token has been revoked",
+                    "auth/insufficient-permission":
+                        "Insufficient permission for the operation",
+                    "auth/internal-error": "Internal error encountered",
+                    "auth/invalid-argument": "Invalid argument provided",
+                    "auth/invalid-disabled-field":
+                        "Invalid disabled user property",
+                    "auth/invalid-display-name":
+                        "Invalid display name property",
+                    "auth/invalid-dynamic-link-domain":
+                        "Invalid dynamic link domain for the project",
+                    "auth/invalid-email": "Invalid email format",
+                    "auth/invalid-email-verified-field":
+                        "Invalid emailVerified user property",
+                    "auth/invalid-hash-algorithm":
+                        "Invalid hash algorithm provided",
+                    "auth/invalid-hash-block-size": "Invalid hash block size",
+                    "auth/invalid-hash-derived-key-length":
+                        "Invalid hash derived key length",
+                    "auth/invalid-hash-key": "Invalid hash key",
+                    "auth/invalid-hash-memory-cost": "Invalid hash memory cost",
+                    "auth/invalid-hash-parallelization":
+                        "Invalid hash parallelization",
+                    "auth/invalid-hash-rounds": "Invalid hash rounds",
+                    "auth/invalid-hash-salt-separator":
+                        "Invalid hash salt separator",
+                    "auth/invalid-id-token": "Invalid ID token",
+                    "auth/invalid-last-sign-in-time":
+                        "Invalid last sign in time",
+                    "auth/invalid-page-token":
+                        "Invalid page token for list operation",
+                    "auth/invalid-password": "Invalid password provided",
+                    "auth/invalid-password-hash": "Invalid password hash",
+                    "auth/invalid-password-salt": "Invalid password salt",
+                    "auth/invalid-phone-number": "Invalid phone number",
+                    "auth/invalid-photo-url": "Invalid photo URL",
+                    "auth/invalid-provider-data": "Invalid provider data",
+                    "auth/invalid-provider-id": "Invalid provider ID",
+                    "auth/invalid-oauth-responsetype":
+                        "Invalid OAuth responseType",
+                    "auth/invalid-session-cookie-duration":
+                        "Invalid session cookie duration",
+                    "auth/invalid-uid": "Invalid user ID",
+                    "auth/maximum-user-count-exceeded":
+                        "Maximum user count exceeded",
+                    "auth/missing-android-pkg-name":
+                        "Missing Android package name",
+                    "auth/missing-continue-uri": "Missing continue URL",
+                    "auth/missing-hash-algorithm": "Missing hash algorithm",
+                    "auth/missing-ios-bundle-id": "Missing iOS Bundle ID",
+                    "auth/missing-uid": "Missing UID",
+                    "auth/operation-not-allowed": "Operation not allowed",
+                    "auth/phone-number-already-exists":
+                        "Phone number already exists",
+                    "auth/project-not-found": "Project not found",
+                    "auth/reserved-claims": "Reserved claims present",
+                    "auth/session-cookie-revoked":
+                        "Session cookie has been revoked",
+                    "auth/too-many-requests": "Too many requests",
+                    "auth/unauthorized-continue-uri":
+                        "Unauthorized continue URI",
                 };
                 setError(
                     errorMessageMap[errorMessage.code] || "An error occurred",
@@ -98,7 +162,7 @@ const AuthForm = ({ variant="login" }: FormVariant) => {
 
     return (
         <div className="flex flex-col items-center justify-center gap-3 font-openSans">
-            <h3 className="z-10 font-playfairDSC text-4xl font-bold capitalize text-[#205041] text-center">
+            <h3 className="z-10 text-center font-playfairDSC text-4xl font-bold capitalize text-[#205041]">
                 {variant === "register"
                     ? "Register your account"
                     : "Login to your account"}
